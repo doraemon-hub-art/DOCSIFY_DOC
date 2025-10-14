87113733aec566601c90fb89adacf10ec66d5273
doraemon-hub-art <1660219734@qq.com>
2025-10-14 21:05:36 +0800

    add js note

---
+++ docs/LearnNotes/JavaScript/TestCode/test.js
@@ -0,0 +1,18 @@
+// 随便打印一些东西
+// console.log("Hello, World!");
+// console.log(1 + 3.14);
+// console.log(16 + "Volvo") // 16Volvo
+
+
+
+// 创建一个简单的 HTTP 服务器
+// const http = require('http');
+
+// const server = http.createServer((req, res) => {
+//     res.writeHead(200, { 'Content-Type': 'text/plain' });
+//     res.end('Hello, Node.js!');
+// });
+
+// server.listen(3000, () => {
+//     console.log('Server running at http://localhost:3000/');
+// });
